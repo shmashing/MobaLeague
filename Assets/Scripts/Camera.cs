661a03769f9934f4d584599825528ab34caae4d8
Kyle <kyle.mcfar1291@gmail.com>
2017-08-29 18:03:09 -0700

    initial commit. player movement, combat, creep pathing and spawn

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera: MonoBehaviour {
+
+	public Transform player;
+	public float turnSpeed = 1.0f;
+
+	private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+		offset = new Vector3 (player.position.x, player.position.y + 2.0f, player.position.z - 5.0f);
+	}
+
+	// Update is called once per frame
+	void LateUpdate () {
+		offset = Quaternion.AngleAxis (Input.GetAxis("Mouse X")*turnSpeed, Vector3.up) * offset;
+		transform.position = player.position + offset;
+		transform.LookAt (player.position);
+	}
+}
+
